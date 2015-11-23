d81608f52a627cf1c1006ccd6b1382a23924c399
Joshua Wood <jwood311@ymail.com>
2015-11-23 18:20:01 -0500

    Update
    Update errors

--- TOBA/src/java/banking/transactionServlet.java
+++ TOBA/src/java/banking/transactionServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package banking;
 
 import java.io.IOException;
@@ -13,11 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Josh
- */
-@WebServlet(name = "transactionServlet", urlPatterns = {"/transactionServlet"})
+
 public class TransactionServlet extends HttpServlet {
 
     /**
